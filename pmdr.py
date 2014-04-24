3d18fc919841a47adcd2ecc6debe4911a0166ce6
Shahin Azad <ishahinism@gmail.com>
2014-04-24 15:39:12 +0430

    Add calltimer function that will call the timer() function based on the status of pomodoro situation.

--- pmdr.py
+++ pmdr.py
@@ -9,12 +9,21 @@ def timer(minute):
     Calling this function will generate a timer for the given minutes and
     wait until it finishes. It'll also generate a printable timer.
     """
-    start_time = time.time()
-    end_time = start_time + (minute * 60)
+    end_time = time.time() + (minute * 60)
     while time.time() < end_time:
         clock = time.strftime("%H:%M:%S", time.localtime(time.time()))
         sys.stdout.write("%s    \r" % (clock))
         sys.stdout.flush()
 
 
-timer(1)
+def calltimer(status, pmdrtime=25, resttime=5, longrest=25):
+    """
+    Call timer based on status which can be in 3 types: pomodoro, rest,
+    longrest.
+    """
+    if status == "pomodoro":
+        timer(pmdrtime)
+    elif status == "rest":
+        timer(resttime)
+    else:
+        timer(longrest)
